a9362c1bf4c6bdc13e7773ae216885972e143e55
Ko-JeaGun <86071357+Gun06@users.noreply.github.com>
2024-05-10 22:06:52 +0900

    Add files via upload

---
+++ main/index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var albumBox = document.getElementById("albumBox");
+    var resultContainer = document.getElementById("resultContainer");
+    var resultFrame = document.getElementById("resultFrame");
+    var overlay = document.getElementById("overlay");
+
+    // 이미지를 클릭했을 때
+    albumBox.addEventListener("click", function(event) {
+        if (event.target.tagName === "IMG") {
+            var imageUrl = event.target.src;
+            resultFrame.src = "/play/play.html"; // result.html로 변경
+            resultContainer.classList.add("show"); // 결과 컨테이너를 보이도록 함
+            overlay.style.display = "block"; // 오버레이를 보이도록 함
+        }
+    });
+
+    // 오버레이를 클릭했을 때
+    overlay.addEventListener("click", function() {
+        resultContainer.classList.remove("show"); // 결과 컨테이너를 숨김
+        overlay.style.display = "none"; // 오버레이를 숨김
+        resultFrame.src = ""; // iframe 초기화
+    });
+});
